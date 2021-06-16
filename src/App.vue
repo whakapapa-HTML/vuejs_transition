8d771424bc6906fcb3b45e032d74d4ca90fd99ce
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-06-16 15:08:41 +0900

    CSSアニメーション

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+      <transition name="fade" appear>
+        <p v-if="show">hello</p>
+      </transition>
+      <transition  name="slide">
+        <div v-if="show">
+          <p>bye</p>
+          <p>hello</p>
+        </div>
+      </transition>
+     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        show: true
+      };
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .fade-enter{
+    opacity:  0;
+  }
+  .fade-enter-active{
+    transition: opacity 0.5s;
+  }
+  .fade-enter-to{
+    opacity: 1;
+  }
+  .fade-leave{
+    opacity: 1;
+  }
+  .fade-leave-active{
+    transition: opacity 0.5s;
+  }
+  .fade-leave-to{
+    opacity:  0;
+  }
+
+  .slide_enter,
+  .slide_leave_to {
+    opacity: 0;
+  }
+
+  .slide_enter-active{
+    animation: slide-in 0.5s;
+  }
+
+  .slide_leave-active{
+    animation: slide-in 0.5s reverse;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(100px);
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+
+  .main {
+    width: 70%;
+    margin: auto;
+    padding-top: 5rem;
+    text-align: center;
+    }
+
 </style>
